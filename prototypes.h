094f712ad964408e1ef46cf6714fb8fd62f94919
stevesarb <sesarber@gmail.com>
2023-10-23 23:48:51 -0700

    forgot prototypes header file

---
+++ prototypes.h
@@ -0,0 +1,18 @@
+#ifndef PROTOTYPES_H
+#define PROTOTYPES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// helpers
+int get_initial_choice();
+int get_file_choice();
+char* get_file_path();
+void select_files_to_process();
+
+// search for files
+char* find_largest_file();
+
+
+#endif
